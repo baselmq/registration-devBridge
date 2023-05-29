15bfebef70b555c1c7066941853d55b8d1388c5b
Basel Qarqa <basel.m.qarqa@gmail.com>
2023-05-29 09:37:27 +0300

    done all

--- js/login.js
+++ js/login.js
@@ -79,14 +79,13 @@ form.addEventListener("submit", (e) => {
     currentObj.status = "login";
     localStorage.setItem(inputEmail.value, JSON.stringify(currentObj));
 
-    // if (currentObj.submit == "submitted") {
-    //   window.location.href = "../pages/report.html";
-    // } else if (currentObj.joinNow == "joined") {
-    //   window.location.href = "../pages/registration_page.html";
-    // } else {
-    //   location.href = form.getAttribute("action");
-    // }
-    location.href = form.getAttribute("action");
+    if (currentObj.submit == "submitted") {
+      window.location.href = "../pages/report.html";
+    } else if (currentObj.joinNow == "joined") {
+      window.location.href = "../pages/registration_page.html";
+    } else {
+      location.href = form.getAttribute("action");
+    }
   }
 });
 
